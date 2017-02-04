a2c0984683de6c24705f0f99c0ef5618d121519b
Shivam Parikh <parikh268@gmail.com>
2017-02-04 12:21:12 -0500

    Gradle Project setup
    Gradle Project created with initial setup and directory setup

---
+++ src/main/java/com/sparikh/projects/simpleMongo/main/Main.java
@@ -0,0 +1,10 @@
+package com.sparikh.projects.simpleMongo.main;
+
+/**
+ * Created by shivamparikh on 2/4/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Work ! Shiv is Gradling TODAY !");
+    }
+}
